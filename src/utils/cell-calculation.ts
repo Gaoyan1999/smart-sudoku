3871ed151c5a0488ce94ff75b0d4d250f7227cbe
高彦 <danielgao@yaocheng.cn>
2024-08-30 11:26:07 +0800

    support resetting current soduku

--- src/utils/cell-calculation.ts
+++ src/utils/cell-calculation.ts
@@ -1,57 +1,58 @@
-import {SudokuCell, SudokuData} from "../types/sudoku.ts";
-import {uniq} from "lodash";
-import {getRelateCells} from "./location.ts";
+import { SudokuCell, SudokuData } from "../types/sudoku.ts";
+import { uniq } from "lodash";
+import { getRelateCells } from "./location.ts";
 
 export function fillAllCandidate(matrix: SudokuData["matrix"]) {
-    matrix.forEach((row, rowIndex) => {
-        row.forEach((cell, colIndex) => {
-            cell.notingCandidates = findMissingNumbers(
-                uniq(
-                    getRelateCells({ rowIndex, colIndex }, matrix)
-                        .filter((cell) => cell.value !== 0)
-                        .map((cell) => cell.value),
-                ),
-            );
-        });
+  console.log("fillAllCandidate");
+  matrix.forEach((row, rowIndex) => {
+    row.forEach((cell, colIndex) => {
+      cell.notingCandidates = findMissingNumbers(
+        uniq(
+          getRelateCells({ rowIndex, colIndex }, matrix)
+            .filter((cell) => cell.value !== 0)
+            .map((cell) => cell.value),
+        ),
+      );
     });
-    return matrix;
+  });
+  return matrix;
 }
 
 function findMissingNumbers(nums: number[]) {
-    const results: number[] = [];
-    for (let i = 1; i <= 9; i++) {
-        if (!nums.includes(i)) {
-            results.push(i);
-        }
+  const results: number[] = [];
+  for (let i = 1; i <= 9; i++) {
+    if (!nums.includes(i)) {
+      results.push(i);
     }
-    return results;
+  }
+  return results;
 }
 
 export function fillCells(mission: string, solution: string): SudokuCell[][] {
-    const result: SudokuCell[][] = [];
-    if (mission.length !== 81 || solution.length !== 81) {
-        throw Error("Invalid input");
-    }
-    const missionRowsString: string[] = [];
-    const solutionRowsString: string[] = [];
-    for (let i = 0; i < 9; i++) {
-        missionRowsString.push(mission.slice(i * 9, (i + 1) * 9));
-        solutionRowsString.push(solution.slice(i * 9, (i + 1) * 9));
-    }
-    missionRowsString.forEach((rowString, rowIndex) => {
-        const row: SudokuCell[] = [];
-        result.push(row);
-        for (let i = 0; i < rowString.length; i++) {
-            const value = +rowString[i];
+  const result: SudokuCell[][] = [];
+  if (mission.length !== 81 || solution.length !== 81) {
+    throw Error("Invalid input");
+  }
+  const missionRowsString: string[] = [];
+  const solutionRowsString: string[] = [];
+  for (let i = 0; i < 9; i++) {
+    missionRowsString.push(mission.slice(i * 9, (i + 1) * 9));
+    solutionRowsString.push(solution.slice(i * 9, (i + 1) * 9));
+  }
+  missionRowsString.forEach((rowString, rowIndex) => {
+    const row: SudokuCell[] = [];
+    result.push(row);
+    for (let i = 0; i < rowString.length; i++) {
+      const value = +rowString[i];
 
-            row.push({
-                value: value,
-                type: value === 0 ? "unknown" : "known",
-                realAnswer: +solutionRowsString[rowIndex][i],
-                notingCandidates: [],
-            });
-        }
-    });
+      row.push({
+        value: value,
+        type: value === 0 ? "unknown" : "known",
+        realAnswer: +solutionRowsString[rowIndex][i],
+        notingCandidates: [],
+      });
+    }
+  });
 
-    return result;
+  return result;
 }
