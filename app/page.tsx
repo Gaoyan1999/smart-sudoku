d4a67ac70b963bf272233e165f259951fd5f3afe
Daniel Gao <danielgao1029@outlook.com>
2024-12-25 00:39:09 +0800

    connect database

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+import { createClient } from "@/app/utils/supabase/server";
+
+export default async function Page() {
+  const supabase = await createClient();
+  const { data } = await supabase.from("sudoku_puzzle").select();
+  console.log(data);
+  return <h1>Hello, Next.js!</h1>;
+}
