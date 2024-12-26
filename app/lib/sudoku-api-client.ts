6dbd7850e4ac561bed8a39c20de9f98928733c0b
Daniel Gao <danielgao1029@outlook.com>
2024-12-26 23:53:48 +0800

    format

--- app/lib/sudoku-api-client.ts
+++ app/lib/sudoku-api-client.ts
@@ -2,7 +2,6 @@ import { Database } from "@/app/types/database.type";
 import { createClient } from "@supabase/supabase-js";
 import { SudokuDifficulty } from "../types/sudoku";
 
-
 const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
