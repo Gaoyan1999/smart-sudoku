777dff9118e4cf38fce1b5517eb000c86ae3f57b
Daniel Gao <1803483451@qq.com>
2025-11-17 13:57:53 +1100

    finish solve puzzle

--- app/sudoku/import/page.tsx
+++ app/sudoku/import/page.tsx
@@ -5,6 +5,11 @@ import { SudokuBody } from '@/app/ui/sudoku/sudoku-body';
 import { KeyboardEventHandler, useState } from 'react';
 import { getMakingNewPuzzleSudoku } from '../sudoku';
 import { throttle } from 'lodash';
+import { NumberInput } from '@/app/ui/sudoku/number-input';
+import { Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { checkSudokuValid } from '@/app/utils/sudoku-utils';
 
 export default function Page() {
   const [sudoku, setSudoku] = useState<Sudoku>(getMakingNewPuzzleSudoku);
@@ -80,11 +85,57 @@ export default function Page() {
       };
     });
   }
+  function handleNumberInput(num: number) {
+    if (!sudoku.context.selectedPosition) return;
+    const { rowIndex, colIndex } = sudoku.context.selectedPosition;
+    setCellValue(rowIndex, colIndex, num);
+  }
+
+  function handleDeleteCell() {
+    if (!sudoku.context.selectedPosition) return;
+    const { rowIndex, colIndex } = sudoku.context.selectedPosition;
+    setCellValue(rowIndex, colIndex, 0);
+  }
+
+  function handleFinishMaking() {
+    const { isValid, errorMessage } = checkSudokuValid(sudoku.data.matrix);
+    if (!isValid) {
+      alert(errorMessage);
+      return;
+    }
+  }
 
   return (
-    <div style={{ outline: 'none' }} tabIndex={1} onKeyDown={handleKeyDown}>
+    <div
+      className="px-2 py-4 flex flex-col md:flex-row h-full relative md:space-x-4 space-y-4 md:space-y-0"
+      style={{ outline: 'none' }}
+      tabIndex={1}
+      onKeyDown={handleKeyDown}
+    >
       <div className="flex-shrink-0 flex-grow">
         <SudokuBody sudoku={sudoku} setPosition={setPosition} />
+      </div>
+      <div className="flex-shrink-0">
+        <div>Tool Area</div>
+        <div className="mt-4 flex flex-col gap-2">
+          <Button
+            variant="outlined"
+            startIcon={<DeleteIcon />}
+            onClick={handleDeleteCell}
+            disabled={!sudoku.context.selectedPosition}
+          >
+            Delete Cell
+          </Button>
+          <Button
+            variant="contained"
+            startIcon={<CheckCircleIcon />}
+            onClick={handleFinishMaking}
+            disabled={sudoku.context.isFinished}
+          >
+            Finish
+          </Button>
+        </div>
+        <NumberInput handleNumberInput={handleNumberInput} />
       </div>
     </div>
   );
